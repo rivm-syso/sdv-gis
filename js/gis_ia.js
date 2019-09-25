054e0201cca0af65889e0e325a1b85e3ab104b4e
Rob Beffers <rob.beffers@rivm.nl>
2019-09-25 10:37:17 +0200

    Eerste commit

--- js/gis_ia.js
+++ js/gis_ia.js
@@ -626,6 +626,7 @@ var gis_ia_filters={
 				}
 			}
 		});
+		jQuery('.gis_ia_filters_submenu').click(function(e) {e.preventDefault();});
     };
     ol.inherits(ol.control.Filter, ol.control.Control);
     var Filter = ol.control.Filter;
