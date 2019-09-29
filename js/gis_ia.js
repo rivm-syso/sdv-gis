ddc7765a3707377f631d3ca0fb7105d66da56aab
Rob Beffers <rob.beffers@rivm.nl>
2019-09-29 10:02:00 +0200

    Eerste commit\

--- js/gis_ia.js
+++ js/gis_ia.js
@@ -697,30 +697,6 @@ var gis_ia_filters={
 			jQuery('#gis_ia_options_button_'+map_id).hide();
         };
         ol.control.Control.call(this, {element: element,target: options.target});
-/*
-		// html opbouwen mbt de filtering
-        var html, checkSameField=GIS_ia_maps[map_id].fg>=1;
-		switch (GIS_ia_maps[map_id].l.substr(0,1)) {
-			case '0': html=gis_ia_filters.html(map_id); break;
-			case '1': html=gis_ia_get_layer_div(map_id)+gis_ia_filters.html(map_id); break;
-			case '2': html=gis_ia_filters.html(map_id)+gis_ia_get_layer_div(map_id); break;
-		}
-		html='<div id="f2-'+map_id+'" onclick="gis_ia_filters_submenuClick('+map_id+',-1);" class="f2"><div id="f1b-'+map_id+'" class=""><div class="gis_ia_filters_close"><button onclick="filterwindowCheckHide('+map_id+');" class="gis_ia_filters_button">X sluiten</button></div></div>'+html+'<div id="f3b-'+map_id+'"><div class="gis_ia_filters_toon"><button onclick="filterwindowCheckHide('+map_id+');" class="gis_ia_filters_button">Toon resultaten</button></div></div></div>';
-		jQuery('#gis_ia_filters_'+map_id).html(html);
-		var inputs=jQuery('#f2-'+map_id+' :input').on('keyup',function(e) {
-			if (e.keyCode==13) {
-				e.preventDefault();
-				var nextInput = inputs.get(inputs.index(this) + 1);
-				if (nextInput) {
-					nextInput.focus();
-				}
-			}
-		});
-		jQuery('.gis_ia_filters_submenu').click(function(e) {
-			e.preventDefault();
-			return false;
-		});
-*/
     };
     ol.inherits(ol.control.Filter, ol.control.Control);
     var Filter = ol.control.Filter;
@@ -1197,20 +1173,8 @@ function GIS_paragraaf_start(map_id) {
         
 	// zorg dat de kaart de juiste afmeting heeft; Maximale breedte op desktop en evt. volledige breedte op mobiel
 	var wpixels=jQuery('#gis_ia_map_'+map_id).parent().width(); // breedte vd kaart
-//	if (wpixels>=GIS_ia_maps[map_id].w) {
-//		jQuery(map.parent()).css({'width': GIS_ia_maps[map_id].w+'px', 'height': (GIS_ia_maps[map_id].w*GIS_ia_maps[map_id].a)+'px','display':'inline-block'});
-		// Evt floating
-// LET OP: DIT MOET NOG WORDEN AFGESTEMD MET DRUPAL-RESPONSIVE-EXPERT		
-//		switch (GIS_ia_maps[map_id].fl) {
-//			case 1: jQuery(map.closest('.field__item')).css({'float':'left','margin':'0 20px 20px 0'}); break;
-//			case 2: jQuery(map.closest('.field__item')).css({'float':'right','margin':'0 0 20px 20px'}); break;
-//		}
-//		wpixels=GIS_ia_maps[map_id].w;
-//	} else {
-//		jQuery(map.parent()).css({'width': wpixels+'px', 'height': (wpixels*GIS_ia_maps[map_id].a)+'px','display':'inline-block'});
-//	}
-
 	jQuery(map.parent()).css({'height': (wpixels*GIS_ia_maps[map_id].a)+'px'});
+	jQuery('#gis_ia_filters_'+map_id).css('width',300+'px');
 
 	// Set de juiste projectie en 'set' proj4. Dit is nodig voor Position2
 	GIS_ia_maps[map_id].projection = new ol.proj.Projection({code: 'EPSG:28992',units: 'm',extent:GIS_ia_maps[map_id].extNL});
