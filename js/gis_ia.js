0f0f1a54a226b17e874d6bbee0b333e2b7ba4231
Rob Beffers <rob.beffers@rivm.nl>
2019-09-29 11:06:45 +0200

    Eerste commit\

--- js/gis_ia.js
+++ js/gis_ia.js
@@ -2059,7 +2059,11 @@ if (typeof(Drupal)!='undefined') {
 						var no=el.prop('id').substr(11);
 /***************
 Voor testdoeleinde: */
-$($($('#gis_ia_base_'+no).parent()).parent()).css({'float:':'right','margin':'0 0 20px 40px','width':'50%'});
+if (parseInt(no)|1==1) {
+	$($($('#gis_ia_base_'+no).parent()).parent()).css({'float':'right','margin':'0 0 20px 40px','width':'50%'});
+} else {
+	$($($('#gis_ia_base_'+no).parent()).parent()).css({'float':'left','margin':'0 40px 20px 0','width':'50%'});
+}
 /***************/
 						GIS_paragraaf_start(parseInt(no,10));
 					});
