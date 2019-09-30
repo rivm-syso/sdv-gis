c70703fc361b8b96d6fcbb2488e64b41fbf13f19
Rob Beffers <rob.beffers@rivm.nl>
2019-09-30 08:59:25 +0200

    Eerste commit\

--- js/gis_ia.js
+++ js/gis_ia.js
@@ -1454,26 +1454,30 @@ function GIS_paragraaf_start(map_id) {
 		html='<div id="f2-'+map_id+'" onclick="gis_ia_filters_submenuClick('+map_id+',-1);" class="f2"><div id="f1b-'+map_id+'" class=""><div class="gis_ia_filters_close"><button onclick="filterwindowCheckHide('+map_id+');" class="gis_ia_filters_button">X sluiten</button></div></div>'+html+'<div id="f3b-'+map_id+'"><div class="gis_ia_filters_toon"><button onclick="filterwindowCheckHide('+map_id+');" class="gis_ia_filters_button">Toon resultaten</button></div></div></div>';
 		jQuery('#gis_ia_filters_'+map_id).html(html).css('width',(parseInt(GIS_ia_maps[map_id].pw.substr(1,1))*20+180)+'px');
 		GIS_ia_maps[map_id].hasFilter=true;
+		// jquery voor filter-panel
+		var inputs=jQuery('#f2-'+map_id+' :input').on('keyup',function(e) {
+			if (e.keyCode==13) {
+				e.preventDefault();
+				var nextInput = inputs.get(inputs.index(this) + 1);
+				if (nextInput) {
+					nextInput.focus();
+				}
+			}
+		});
+		jQuery('.gis_ia_filters_submenu').click(function(e) {
+			e.preventDefault();
+			return false;
+		});
 	} else {
 		GIS_ia_maps[map_id].hasFilter=false;
 	}
-	var inputs=jQuery('#f2-'+map_id+' :input').on('keyup',function(e) {
-		if (e.keyCode==13) {
-			e.preventDefault();
-			var nextInput = inputs.get(inputs.index(this) + 1);
-			if (nextInput) {
-				nextInput.focus();
-			}
-		}
-	});
-	jQuery('.gis_ia_filters_submenu').click(function(e) {
-		e.preventDefault();
-		return false;
-	});
 
 	// Filter
-	var filter = new ol.control.Filter({'map_id':map_id,className:'gis_ia_filter'+(GIS_ia_maps[map_id].p==0?'':' gis_ia_filter1')});
-	GIS_ia_maps[map_id].map.addControl(filter);
+	if (GIS_ia_maps[map_id].hasFilter) {
+		var filter = new ol.control.Filter({'map_id':map_id,className:'gis_ia_filter'+(GIS_ia_maps[map_id].p==0?'':' gis_ia_filter1')});
+		GIS_ia_maps[map_id].map.addControl(filter);
+	}
+	
 	/* knoppen linksonder */
 	if (GIS_ia_maps[map_id].l1 == 1) {
         GIS_ia_maps[map_id].map.addControl(new ol.control.Legenda({'map_id': map_id}));
