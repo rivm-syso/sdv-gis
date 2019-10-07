3bdec060ecdd18a65c8defcb64f7d68885c09cf1
Rob Beffers <rob.beffers@rivm.nl>
2019-10-07 14:45:06 +0200

    1e commit

--- js/gis_ia.js
+++ js/gis_ia.js
@@ -1890,11 +1890,11 @@ function GIS_paragraaf_start(map_id) {
 				// daarom huidige size opslaan en vertraagde functie fullscreenChange instellen
 				var map=jQuery('#gis_ia_map_'+map_id);
 				GIS_ia_maps[map_id].fullscreenPrevSize=[map.width(),map.height()];
-//				jQuery(el).on('click',function(e) {
-//					setTimeout('fullscreenChange('+map_id+');',50);
-//				});
+				jQuery(el).on('click',function(e) {
+					setTimeout('fullscreenChange('+map_id+');',3000);
+				});
 			});
-jQuery(jQuery('#gis_ia_map_'+map_id).parent()).on('resize',function() {fullscreenChange(map_id);});
+//jQuery(jQuery('#gis_ia_map_'+map_id).parent()).on('resize',function() {fullscreenChange(map_id);});
 		}
 		GIS_ia_maps[map_id].map.on('movestart', function(evt) {
 			jQuery('#gis_ia_map_'+map_id+'_data').hide();
