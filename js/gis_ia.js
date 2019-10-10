52b13a9577de6668a8a8f5f084867417bad77919
Rob Beffers <rob.beffers@rivm.nl>
2019-10-10 10:50:17 +0200

    1e commit

--- js/gis_ia.js
+++ js/gis_ia.js
@@ -330,10 +330,10 @@ gis_ia_filter.prototype.change=function(id,i) {
 							el=jQuery(x_buttons[t]);
 							id=el.prop('id');
 							if (id==this.ID()+'-xx-van') {
-								if (i==0) {el.show();}
+								if (i==0) {el.show();} else {el.hide();}
 							} else {
 								if (id==this.ID()+'-xx-tot') {
-									if (i==1) {el.show();}
+									if (i==1) {el.show();} else {el.hide();}
 								} else {
 									el.show();
 								}
