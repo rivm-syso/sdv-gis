d479ec1925233c317bb3db43201c259db2350362
Rob Beffers <rob.beffers@rivm.nl>
2019-10-12 10:51:01 +0200

    1e commit

--- js/gis_ia_edit.js
+++ js/gis_ia_edit.js
@@ -342,9 +342,7 @@ function gis_ia_toggle_filters(el) {
 	if (typeof(el)!='undefined') {
 		var els=jQuery(jQuery(jQuery(el).parent()).parent()).children(),t;
 		for (t=0;t<els.length;t++) {
-			if (jQuery(els[t]).hasClass('gis_ia_hideable')) {
-				jQuery(els[t]).toggle();
-			}
+			jQuery(els[t]).find('.gis_ia_hideable').toggle();
 		}
 	} else {
 		if (gis_ia_toggle_filters_) {
